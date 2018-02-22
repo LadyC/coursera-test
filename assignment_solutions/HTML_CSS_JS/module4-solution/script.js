1eb004d09f48b756331e98b7b7f8c20aeba22fde
Chiara Sammarco <chiarasammarco13@gmail.com>
2018-02-22 15:16:22 +0300

    Module 4 solution assignment

--- assignment_solutions/HTML_CSS_JS/module4-solution/script.js
+++ assignment_solutions/HTML_CSS_JS/module4-solution/script.js
@@ -1,8 +1,6 @@
 (function () {
 
-var names = ["Chiara", "Giusy", "Jo", "Justine", "Jennifer", "Federica", 
-              "Laurence", "Paola", "Laura", "Jim", "Serena", "Gabriele",
-              "Johnny", "Jerry", "Michele"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
